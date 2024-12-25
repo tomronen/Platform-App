ff657fe12a69f26b8610a3aba4eb89cf81ed9943
Tom Ronen <tom@runixer.net>
2024-12-25 17:17:48 +0200

    Update imports to use AWS CDK v2 modules
    * **cdk/bin/platform-app.ts**
      - Change import from '@aws-cdk/core' to 'aws-cdk-lib'
    * **cdk/lib/platform-app-stack.ts**
      - Change imports from '@aws-cdk/core', '@aws-cdk/aws-eks', and '@aws-cdk/aws-iam' to 'aws-cdk-lib' and 'constructs'
      - Update constructor to use 'Construct' from 'constructs' instead of 'cdk.Construct'

--- cdk/lib/platform-app-stack.ts
+++ cdk/lib/platform-app-stack.ts
@@ -1,6 +1,7 @@
-import * as cdk from '@aws-cdk/core';
-import * as eks from '@aws-cdk/aws-eks';
-import * as iam from '@aws-cdk/aws-iam';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as eks from 'aws-cdk-lib/aws-eks';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 interface PlatformAppStackProps extends cdk.StackProps {
   clusterName: string;
@@ -8,7 +9,7 @@ interface PlatformAppStackProps extends cdk.StackProps {
 }
 
 export class PlatformAppStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props: PlatformAppStackProps) {
+  constructor(scope: Construct, id: string, props: PlatformAppStackProps) {
     super(scope, id, props);
 
     const cluster = new eks.Cluster(this, 'Cluster', {
